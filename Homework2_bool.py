62a684a74b67fd4351c6d2a7bd7a4dead10fa918
Natiru <chartolaninatia@gmail.com>
2023-04-02 23:09:58 +0200

    update

---
+++ Homework2_bool.py
@@ -0,0 +1,58 @@
+sister_Anna = True
+brother_Georg = False
+print(f'{sister_Anna and brother_Georg =} ')
+
+
+nice_weather = True
+bad_weather = True  
+print(f'{nice_weather and bad_weather =}')
+
+blue_sky = True
+dark_sky = False 
+print(f'{blue_sky and dark_sky =}')
+
+strong_alcohol = False
+old_alcohol = True
+print(f'{strong_alcohol and old_alcohol =}')
+
+
+dirty_water = False
+clear_water = False 
+print(f'{dirty_water and clear_water =}')
+
+
+
+Kate_is_beautiful = True
+John_is_ugly = False 
+print(f'Kate_is_Beautiful: {Kate_is_beautiful}', f'John_is_ugly: {John_is_ugly}')
+print(f'Kate_is_Beautiful= {Kate_is_beautiful} John_is_ugly= {John_is_ugly}')
+
+Sandra_is_older = True
+David_is_younger = False
+print(f'{Sandra_is_older or David_is_younger =}')
+
+book_hotel = True
+went_Museum = False
+print(f'{book_hotel or went_Museum =}')
+
+ate_sushi = False
+drank_sake = False
+print(f'{ate_sushi or drank_sake =}')
+
+made_party = True
+had_fun = True
+print(f'{made_party or had_fun =}')
+
+came_later = True
+went_earlier = False
+print(f'{came_later or not went_earlier =}')
+
+ate_ice = False
+drank_cola = True
+print(f'{ate_ice or not drank_cola =}')
+
+baked_bread = False 
+baked_tort = True
+print(f'{baked_bread or not baked_tort =}')
+
+print()
